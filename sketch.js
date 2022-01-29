abd0da19dfe1d9b131befcfd3cc4a4786ca9f870
Nehal-Bothra <86347365+Nehal-Bothra@users.noreply.github.com>
2022-01-29 20:47:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,113 +1,44 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-var engine, world, backgroundImg;
-var canvas, angle, tower, ground, cannon;
-var balls = [];
-var boats = [];
-var boatAnimation = [];
-var boatSpritedata, boatSpritesheet;
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
 
-function preload() {
-  backgroundImg = loadImage("./assets/background.gif");
-  towerImage = loadImage("./assets/tower.png");
-  boatSpritedata =loadJSON("assets/boat/boat.json");
-  boatSpritesheet = loadImage("assets/boat/boat.png");
-  
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  groundImage = loadImage("ground2.png")
 }
 
 function setup() {
-  canvas = createCanvas(1200,600);
-  engine = Engine.create();
-  world = engine.world;
-  angle = -PI / 4;
-  ground = new Ground(0, height - 1, width * 2, 1);
-  tower = new Tower(150, 350, 160, 310);
-  cannon = new Cannon(180, 110, 100, 50, angle);
-  boatFrames = boatSpritedata.frames;
-  for(var i =0; i<boatFrames.length;i++){
-var pos =boatFrames[i].position;
-var img = boatSpritesheet.get(pos.x,pos.y,pos.w,pos.h);
-boatAnimation.push(img);
-  }
-
+  createCanvas(600, 200);
   
-
+  //create a trex sprite
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
   
+  //adding scale and position to trex
+  trex.scale = 0.5;
+  trex.x = 50
+  
+  //create ground sprite
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage)
+ 
 }
 
 function draw() {
-  showBoats()
-  background(189);
-  image(backgroundImg, 0, 0, width, height);
-
- 
-
-  Engine.update(engine);
-  ground.display();
-showBoats();
- 
-
-  for (var i = 0; i < balls.length; i++) {
-    showCannonBalls(balls[i], i);
+  background(220);
+ ground.velocityX=-2
+ console.log(ground.x)
+ if(ground.x<0){
+   ground.x=ground.width/2
+ }
+  //jumping the trex on space key press
+  if(keyDown("space")) {
+    trex.velocityY = -10;
   }
-
-  cannon.display();
-  tower.display();
-
   
-}
-
-function keyPressed() {
-  if (keyCode === DOWN_ARROW) {
-    var cannonBall = new CannonBall(cannon.x, cannon.y);
-    balls.push(cannonBall);
-  }
-}
-
-//function to show the ball
-function showCannonBalls(ball, index) {
-  ball.display();
-  if (ball.body.position.x >= width || ball.body.position.y >= height - 50) {
-    Matter.World.remove(world, ball.body);
-    balls.splice(index, 1);
-  }
-}
-
+  trex.velocityY = trex.velocityY + 0.8
+  
  
-
-    function showBoats() {
-      if (boats.length > 0) {
-        if (
-          boats.length < 4 &&
-          boats[boats.length - 1].body.position.x < width - 300
-        ) {
-          var positions = [-130, -100, -120, -80];
-          var position = random(positions);
-          var boat = new Boat(width,height - 100, 200, 200, position,boatAnimation);
-          boats.push(boat);
-        }
-    
-        for (var i = 0; i < boats.length; i++) {
-          Matter.Body.setVelocity(boats[i].body, {
-            x: -0.9,
-            y: 0
-          });
-    
-          boats[i].display();
-          boats[i].animate();
-        }
-      } else {
-        var boat = new Boat(width, height - 100, 200, 200, -100);
-        boats.push(boat);
-      }
-    }
-function keyReleased() {
-  if (keyCode === DOWN_ARROW) { 
-    balls[balls.length - 1].shoot();
-  }
+ //stop trex from falling down 
+  trex.collide(ground);
+  drawSprites();
 }
-
-
-
